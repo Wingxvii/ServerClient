dac43a702e87f81bb6fba9a7772d41bcc50da8b0
John <john.wang@uoit.net>
2019-10-31 17:36:57 -0400

    more bug fixes

--- Server/Server/ServerNetwork.cpp
+++ Server/Server/ServerNetwork.cpp
@@ -157,11 +157,11 @@ void ServerNetwork::sendTo(Packet pack, int clientID)
 
 	pack.serialize(packet_data);
 
-	int sendOK = sendto(in, packet_data, packet_size, 0, (sockaddr*)& ConnectedUsers[clientID - 1].clientAddress, ConnectedUsers[clientID - 1].clientLength);
+	int sendOK = sendto(in, packet_data, packet_size, 0, (sockaddr*)& ConnectedUsers[clientID].clientAddress, ConnectedUsers[clientID - 1].clientLength);
 	if (sendOK == SOCKET_ERROR) {
 		cout << "Send Error: " << WSAGetLastError() << endl;
 	}
-
+	
 }
 
 void ServerNetwork::relay(Packet pack, int clientID)
